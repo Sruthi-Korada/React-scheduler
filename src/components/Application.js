1bc3ee5dbafa3c1a5749ee8478caed57f96a75fd
sruthikorada <sruthikorada36@gmail.com>
2020-03-18 18:36:27 -0700

    using axios and useEffect to fetch data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,71 @@
-import React from "react";
-
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
+import DayList from "components/DayList";
+import Appointment from "components/Appointment/index";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm"
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm"
+  },
+  {
+    id: 4,
+    time: "5pm"
+  }
+];
 
 export default function Application(props) {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const setDay = days => setState(prev => ({ ...prev, days }));
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then(res => {
+      console.log(res.data);
+      setDay(res.data);
+    });
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-      className="sidebar--centered"
-      src="images/logo.png"
-      alt="Interview Scheduler"
-    />
-    <hr className="sidebar__separator sidebar--centered" />
-    <nav className="sidebar__menu"></nav>
-    <img
-      className="sidebar__lhl sidebar--centered"
-      src="images/lhl.png"
-      alt="Lighthouse Labs"
-    />
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+        <DayList days={state.days} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment key={appointment.id} interview={appointment.interview} />
+        ))}
       </section>
     </main>
   );
